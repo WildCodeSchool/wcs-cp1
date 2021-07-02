f4668f08a3a35b2218b1f80d8c04036c63ed9707
BenSbnt <b.bonnet97@hotmail.fr>
2021-07-02 11:09:05 +0200

    exo-3

--- algo/3/theaterSieges.js
+++ algo/3/theaterSieges.js
@@ -16,7 +16,18 @@ Au final la liste devra ressembler à :
 */
 
 function theaterSieges() {
-  // Your code here !
+
+//   const colonne = [1,26];{
+//   const sieges = [1,100];
+// for (let  )
+//   }
+const colonne = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26] 
+const sieges = []
+for (let i = 1 ; i <= 100; i++ ){
+  sieges.push(i);
+}
+const place = colonne.map(num => sieges.map(sieges => `${num}-${sieges}`));
+return place
 }
 
 console.log(theaterSieges());
