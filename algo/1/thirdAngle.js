6720abd47bee01dc9ba7fcde63c531b04fa709bf
Kameli71 <karim.kameli71@gmail.com>
2021-07-02 10:24:58 +0200

    J1 Terminé fixed

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,9 +7,7 @@ thirdAngle(20, 80) doit renvoyer 80
 */
 
 function thirdAngle(a, b) {
-  return 180 - (a + b); 
+  return 180-(a + b) 
 }
-console.log(thirdAngle(90, 30));
-console.log(thirdAngle(20, 80));
 
 module.exports = thirdAngle;
