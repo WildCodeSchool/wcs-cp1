49f97fe7528dbac8d0b4297006a8271ff107f006
sarah <sarah.duchaussoy@hotmail.fr>
2021-07-02 10:25:07 +0200

    Algo 1 done

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,11 @@ thirdAngle(20, 80) doit renvoyer 80
 */
 
 function thirdAngle(a, b) {
+  let i = 180; 
+  return i-=(a+b);
   // Your code here !
 }
+ console.log(thirdAngle(90,30));
+ console.log(thirdAngle(20,80));
 
 module.exports = thirdAngle;
