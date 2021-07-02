341fc9bf04558a0fd6475c39197359929d5efd34
Kameli71 <karim.kameli71@gmail.com>
2021-07-02 13:37:36 +0200

    CSS JS2 terminé

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,13 +1,15 @@
-let avatar = document.querySelector('.avatar');
-let button = document.getElementById('button')
+const avatar = document.querySelector('.avatar');
+const button = document.getElementById('button')
 
 avatar.addEventListener ("click", () => {
     avatar.src = "./image/avatar.svg"
 });
 
-button.addEventListener('click', () =>{
+button.addEventListener('click', (color) =>{
     const name = document.getElementById('name');
-
+    
     let change = prompt('Choose your name');
     name.innerHTML = change;
+
 })
+
