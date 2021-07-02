05d663f2e9f59aca4f6c024bd582b65b75204c65
Yoann <maulineyoann@gmail.com>
2021-07-02 13:58:08 +0200

    bonus js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,9 +1,11 @@
 const avatar = document.querySelector('.avatar');
 const button = document.getElementById('button');
-const background = document.querySelector('.pink-bg');
-const color = document.querySelectorAll('.pink-test');
+const background = document.querySelectorAll('.pink-bg');
+const color = document.querySelectorAll('.pink-text');
+const lien = document.getElementsByTagName('a');
+const modif = document.getElementById('modify');
 
-
+console.log(lien);
 
 avatar.addEventListener('click', () => {
     
@@ -13,12 +15,28 @@ avatar.addEventListener('click', () => {
 button.addEventListener('click', () => {
     
     const name = document.getElementById('name');
-    
-    let change = prompt('Choose your name');
-    color.style.color = "#750ff7";
-    
-    name.innerHTML = change;
+    let changeName = prompt('Choose your name');
+    let changeBG = prompt('Choose your color')
+    color.forEach(element => {
+        element.style.color ="#750ff7";
+    }) 
+    background.forEach(bg => {
+        bg.style.backgroundColor = changeBG;
+    })
+
+    name.innerHTML = changeName;
     name.style.color = "#FFF"
-    background.style.backgroundColor = "#750ff7";
-    
+    background.style.backgroundColor = "#750ff7";  
 })
+
+modif.addEventListener('click', () => {
+    let prompt = prompt("You can modify");
+    const canModif = document.querySelectorAll('modif');
+
+    canModif.forEach(change => {
+        canModif.innerHTML = prompt;
+    })
+})
+
+
+
